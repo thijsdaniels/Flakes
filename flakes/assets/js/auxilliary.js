5f81f11613f5462eed32cbfb130646e12c48b9bf
Thijs Daniels <thijsdaniels89@gmail.com>
2013-02-25 00:05:39 +0100

    Reverted to before i made wrapper divs.
    The wrappers made the code very messy and a the containers would resize
    weirdly during and after switching around, so I reverted. Instead of
    doing it with the wrapper method, use the jScrollPane jQuery plugin.
    It's supported cross browser and fully css stylable.

--- flakes/assets/js/auxilliary.js
+++ flakes/assets/js/auxilliary.js
@@ -3,7 +3,7 @@ var maxDepth = 15;
 var mouseX = Math.round($(window).width() * 0.5);
 var mouseY = Math.round($(window).height() * 0.5);
 var currentContainer;
-var openContainerSize = 600;
+var openContainerSize = '500px';
 var animationDuration = 400;
 var animationDelay = 100;
 
@@ -97,60 +97,65 @@ function switchContainer(newContainer) {
 		// prepare
 		var oldContainer = currentContainer;
 		currentContainer = null;
-		$(newContainer).children('div.wrapper').removeClass('link');
-		$(newContainer).css('overflow', 'hidden');
-		$(oldContainer).css('overflow', 'hidden');
+		$(newContainer).removeClass('link');
 		
 		// fade out content
-		$(oldContainer).children('div.wrapper').children('div#back').animate({opacity: 0}, animationDuration, function() {
-			$(oldContainer).children('div.wrapper').children('div#back').css('display', 'none');
-			$(oldContainer).children('div.wrapper').css({'width': '100%', 'height': '100%'});
+		$(oldContainer).children('div#back').animate({opacity: 0}, animationDuration, function() {
+			$(oldContainer).children('div#back').css('display', 'none');
 		});
-		$(newContainer).children('div.wrapper').children('div#front').animate({opacity: 0}, animationDuration, function() {
-			$(newContainer).children('div.wrapper').children('div#front').css('display', 'none');
-			$(newContainer).children('div.wrapper').css({'width': '100%', 'height': '100%'});
+		$(newContainer).children('div#front').animate({opacity: 0}, animationDuration, function() {
+			$(newContainer).children('div#front').css('display', 'none');
 		});
 		
 		// move and resize
+		var oldContainerSize = $(oldContainer).attr('size') + 'px';
 		setTimeout(function() {
 			$(oldContainer).animate({
-				width: $(oldContainer).attr('size') + 'px',
-				height: $(oldContainer).attr('size') + 'px',
-			}, animationDuration);
-			$(oldContainer).animate({
+				width: oldContainerSize,
+				height: oldContainerSize,
 				left: $(newContainer).css('left'),
 				top: $(newContainer).css('top'),
 				zIndex: $(newContainer).css('z-index'),
 				marginLeft: $(newContainer).css('margin-left'),
 				marginTop: $(newContainer).css('margin-top'),
-			}, {duration: animationDuration, easing: 'easeInQuad', queue: false});
+			}, {'duration': animationDuration, specialEasing: {
+				width: 'swing',
+				height: 'swing',
+				left: 'easeInQuad',
+				top: 'easeInQuad',
+				zIndex: 'easeInQuad',
+				marginLeft: 'easeInQuad',
+				marginTop: 'easeInQuad',
+			}});
 			$(newContainer).animate({
-				width: openContainerSize + 'px',
-				height: openContainerSize + 'px',
-			}, animationDuration);
-			$(newContainer).animate({
+				width: openContainerSize,
+				height: openContainerSize,
 				left: $(oldContainer).css('left'),
 				top: $(oldContainer).css('top'),
 				zIndex: $(oldContainer).css('z-index'),
 				marginLeft: $(oldContainer).css('margin-left'),
 				marginTop: $(oldContainer).css('margin-top'),
-			}, {duration: animationDuration, easing: 'easeInQuad', queue: false});
+			}, {'duration': animationDuration, specialEasing: {
+				width: 'swing',
+				height: 'swing',
+				left: 'easeInQuad',
+				top: 'easeInQuad',
+				zIndex: 'easeInQuad',
+				marginLeft: 'easeInQuad',
+				marginTop: 'easeInQuad',
+			}});
 			
-			// fade in content
+			// fade int
 			setTimeout(function() {
-				$(oldContainer).children('div.wrapper').css({'width': 'auto', 'height': 'auto'});
-				$(newContainer).children('div.wrapper').css({'width': 'auto', 'height': 'auto'});
-				$(oldContainer).children('div.wrapper').children('div#front').css('display', 'block');
-				$(newContainer).children('div.wrapper').children('div#back').css('display', 'block');
-				$(oldContainer).children('div.wrapper').children('div#front').animate({opacity: 1}, animationDuration);
-				$(newContainer).children('div.wrapper').children('div#back').animate({opacity: 1}, animationDuration);
+				$(oldContainer).children('div#front').css('display', 'block');
+				$(newContainer).children('div#back').css('display', 'block');
+				$(oldContainer).children('div#front').animate({opacity: 1}, animationDuration);
+				$(newContainer).children('div#back').animate({opacity: 1}, animationDuration);
 				
 				// finalize
 				setTimeout(function() {
-					$(newContainer).css('overflow', 'auto');
-					$(oldContainer).css('overflow', 'auto');
 					currentContainer = newContainer;
-					$(oldContainer).children('div.wrapper').addClass('link');
+					$(oldContainer).addClass('link');
 				}, animationDuration);
 				
 			}, animationDuration + animationDelay);
@@ -162,31 +167,23 @@ function switchContainer(newContainer) {
 
 function placeContainers() {
 	
-	var logoSize = ($('div#logo').attr('size'));
+	var logoSize = parseInt($('div#logo').css('height'));
 	var logoX = Math.round(($(window).width() * 0.15) - (logoSize * 0.5));
 	var logoY = Math.round(($(window).height() * 0.25) - (logoSize * 0.5));
 	$('div#logo').css({
 		'left': logoX,
 		'top': logoY,
 	});
-	$('div#logo').css({
-		'width': logoSize + 'px',
-		'height': logoSize + 'px',
-	});
 	$('div#logo').bind('click', function() {
 		switchContainer($('div#logo').get());
 	});
 	
-	var pitchSize = ($('div#pitch').attr('size'));
-	var pitchX = Math.round(($(window).width() * 0.5) - (openContainerSize * 0.5));
-	var pitchY = Math.round(($(window).height() * 0.5) - (openContainerSize * 0.5));
+	var pitchSize = parseInt($('div#pitch').css('height'));
+	var pitchX = Math.round(($(window).width() * 0.5) - (pitchSize * 0.5));
+	var pitchY = Math.round(($(window).height() * 0.5) - (pitchSize * 0.5));
 	$('div#pitch').css({
 		'left': pitchX,
 		'top': pitchY,
-	});
-	$('div#pitch').css({
-		'width': openContainerSize + 'px',
-		'height': openContainerSize + 'px',
 	});
 	$('div#pitch div#front').css({'display': 'none', 'opacity': 0});
 	$('div#pitch div#back').css({'display': 'block', 'opacity': 1});
@@ -194,31 +191,23 @@ function placeContainers() {
 		switchContainer($('div#pitch').get());
 	});
 	
-	var contactSize = ($('div#contact').attr('size'));
+	var contactSize = parseInt($('div#contact').css('height'));
 	var contactX = Math.round(($(window).width() * 0.85) - (contactSize * 0.5));
 	var contactY = Math.round(($(window).height() * 0.75) - (contactSize * 0.5));
 	$('div#contact').css({
 		'left': contactX,
 		'top': contactY,
 	});
-	$('div#contact').css({
-		'width': contactSize + 'px',
-		'height': contactSize + 'px',
-	});
 	$('div#contact').bind('click', function() {
 		switchContainer($('div#contact').get());
 	});
 	
-	var showcaseSize = ($('div#showcase').attr('size'));
+	var showcaseSize = parseInt($('div#showcase').css('height'));
 	var showcaseX = Math.round(($(window).width() * 0.1) - (showcaseSize * 0.5));
 	var showcaseY = Math.round(($(window).height() * 0.85) - (showcaseSize * 0.5));
 	$('div#showcase').css({
 		'left': showcaseX,
 		'top': showcaseY,
-	});
-	$('div#showcase').css({
-		'width': showcaseSize + 'px',
-		'height': showcaseSize + 'px',
 	});
 	$('div#showcase').bind('click', function() {
 		switchContainer($('div#showcase').get());
@@ -233,12 +222,12 @@ function updateFlakeCounter() {
 
 function bindProjectDescriptions() {
 	$('div.project').bind('mouseover', function(event) {
-		$(this).children('div.title').stop().animate({'opacity': 0}, 300);
-		$(this).children('div.description').stop().animate({'opacity': 0.9}, 300);
+		$(this).children('div.title').animate({'opacity': 0}, 300);
+		$(this).children('div.description').animate({'opacity': 0.9}, 300);
 	});
 	$('div.project').bind('mouseout', function(event) {
-		$(this).children('div.title').stop().animate({'opacity': 0.9}, 300);
-		$(this).children('div.description').stop().animate({'opacity': 0}, 300);
+		$(this).children('div.title').animate({'opacity': 0.9}, 300);
+		$(this).children('div.description').animate({'opacity': 0}, 300);
 	});
 }
 
